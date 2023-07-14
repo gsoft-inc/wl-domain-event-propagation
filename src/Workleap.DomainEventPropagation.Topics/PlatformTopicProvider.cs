eef82c91dc0fb605e4d2953f254c42111aed4786
Guillaume Caya-Letourneau <guillaume.caya.letourneau@gmail.com>
2023-07-14 11:29:25 -0400

    IDP-443 - Added the topics project (#6)
    * Added the topics project
    
    * Fix ma conflict
    
    * Fixed missing Domain in sln
    
    * Missign readme reference
    
    * Added codeowners
    
    * Added public analyzers files
    
    * Proper team on codeowners

---
+++ src/Workleap.DomainEventPropagation.Topics/PlatformTopicProvider.cs
@@ -0,0 +1,26 @@
+﻿namespace Workleap.DomainEventPropagation;
+
+public sealed class PlatformTopicProvider : ITopicProvider
+{
+    private static readonly Dictionary<string, string> Topics = new()
+    {
+        { "Organization", "-organization-egt-" },
+        { "Integrations", "-integrations-egt-" },
+        { "Signup", "-signup-egt-" },
+    };
+
+    public IEnumerable<string> GetAllTopicsNames()
+    {
+        return Topics.Keys;
+    }
+
+    public IEnumerable<string> GetAllTopicValidationPatterns()
+    {
+        return Topics.Values;
+    }
+
+    public string GetTopicValidationPattern(string topicName)
+    {
+        return Topics[topicName];
+    }
+}
