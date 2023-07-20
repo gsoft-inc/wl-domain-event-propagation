84ca167d4edccacee0d63d73ba51ca1c5d6118df
Félix Pelletier <felix.pelletier@gsoft.com>
2023-07-19 22:28:08 -0400

    [IDP-453] feat(refactor): Event Grid publisher client setup (#10)
    * feat(refactor): setup the Event Grid publisher client from DI and adjust existing event propagation client
    
    * Apply suggestions from code review
    
    Co-authored-by: Guillaume Caya-Letourneau <guillaume.caya.letourneau@gmail.com>
    
    ---------
    
    Co-authored-by: Guillaume Caya-Letourneau <guillaume.caya.letourneau@gmail.com>

--- src/Workleap.DomainEventPropagation.Publishing/EventPropagationClient.cs
+++ src/Workleap.DomainEventPropagation.Publishing/EventPropagationClient.cs
@@ -1,7 +1,6 @@
 using System.Text.Json;
-using Azure;
-using Azure.Core;
 using Azure.Messaging.EventGrid;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Options;
 using Workleap.DomainEventPropagation.Exceptions;
 
@@ -10,25 +9,28 @@ namespace Workleap.DomainEventPropagation;
 /// <summary>
 /// https://github.com/Azure/azure-sdk-for-net/blob/master/sdk/eventgrid/Azure.Messaging.EventGrid/README.md
 /// </summary>
-internal class EventPropagationClient : IEventPropagationClient
+internal sealed class EventPropagationClient : IEventPropagationClient
 {
     private static readonly JsonSerializerOptions SerializerOptions = new();
-
+    
     private readonly EventPropagationPublisherOptions _eventPropagationPublisherOptions;
+    private readonly IAzureClientFactory<EventGridPublisherClient> _eventGridPublisherClientFactory;
     private readonly ITelemetryClientProvider _telemetryClientProvider;
 
     public EventPropagationClient(
+        IAzureClientFactory<EventGridPublisherClient> eventGridPublisherClientFactory,
         IOptions<EventPropagationPublisherOptions> eventPropagationPublisherOptions,
         ITelemetryClientProvider telemetryClientProvider)
     {
         this._eventPropagationPublisherOptions = eventPropagationPublisherOptions.Value;
+        this._eventGridPublisherClientFactory = eventGridPublisherClientFactory;
         this._telemetryClientProvider = telemetryClientProvider;
     }
 
-    public async Task PublishDomainEventAsync(string subject, IDomainEvent domainEvent, CancellationToken cancellationToken)
-    {
-        await this.PublishDomainEventsAsync(subject, new[] { domainEvent }, cancellationToken);
-    }
+    private string TopicName => this._eventPropagationPublisherOptions.TopicName;
+
+    public Task PublishDomainEventAsync(string subject, IDomainEvent domainEvent, CancellationToken cancellationToken)
+        => this.PublishDomainEventsAsync(subject, new[] { domainEvent }, cancellationToken);
 
     public Task PublishDomainEventAsync<T>(T domainEvent, CancellationToken cancellationToken) where T : IDomainEvent
         => this.PublishDomainEventAsync(typeof(T).FullName, domainEvent, cancellationToken);
@@ -38,49 +40,37 @@ internal class EventPropagationClient : IEventPropagationClient
 
     public async Task PublishDomainEventsAsync(string subject, IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken)
     {
-        var topicName = this._eventPropagationPublisherOptions.TopicName;
-        var topicEndpoint = this._eventPropagationPublisherOptions.TopicEndpoint;
-        var topicAccessKey = this._eventPropagationPublisherOptions.TopicAccessKey;
-
-        var topicEndpointUri = new Uri(topicEndpoint);
-        var topicCredentials = new AzureKeyCredential(topicAccessKey);
-        var client = new EventGridPublisherClient(
-            topicEndpointUri,
-            topicCredentials,
-            new EventGridPublisherClientOptions
-            {
-                Retry = { Mode = RetryMode.Fixed, MaxRetries = 1, NetworkTimeout = TimeSpan.FromSeconds(4) }
-            });
+        var domainEventTypes = TelemetryHelper.GetDomainEventTypes(domainEvents);
 
         try
         {
-            var eventGridEvents = GetEventsList(topicName, subject, domainEvents);
+            var eventGridEvents = this.GetEventsList(subject, domainEvents);
 
-            await client.SendEventsAsync(eventGridEvents, cancellationToken);
+            await this._eventGridPublisherClientFactory.CreateClient(EventPropagationPublisherOptions.ClientName).SendEventsAsync(eventGridEvents, cancellationToken);
 
-            this._telemetryClientProvider.TrackEvent(TelemetryConstants.DomainEventsPropagated, $"Propagated domain event with subject '{subject}' on topic '{topicName}'", TelemetryHelper.GetDomainEventTypes(domainEvents));
+            this._telemetryClientProvider.TrackEvent(TelemetryConstants.DomainEventsPropagated, $"Published domain event with subject '{subject}' on topic '{this.TopicName}'", domainEventTypes);
         }
         catch (Exception ex)
         {
             var exception = new EventPropagationPublishingException("An error occured while publishing events to EventGrid", ex)
             {
-                TopicName = topicName,
+                TopicName = this.TopicName,
                 Subject = subject,
-                TopicEndpoint = topicEndpoint
+                TopicEndpoint = this._eventPropagationPublisherOptions.TopicEndpoint
             };
 
-            this._telemetryClientProvider.TrackEvent(TelemetryConstants.DomainEventsPropagationFailed, $"Domain event propagation failed with subject '{subject}' on topic '{topicName}'", TelemetryHelper.GetDomainEventTypes(domainEvents));
+            this._telemetryClientProvider.TrackEvent(TelemetryConstants.DomainEventsPropagationFailed, $"Failed to publish domain event with subject '{subject}' on topic '{this.TopicName}'", domainEventTypes);
             this._telemetryClientProvider.TrackException(exception);
 
             throw exception;
         }
     }
 
-    private static IEnumerable<EventGridEvent> GetEventsList(string topic, string subject, IEnumerable<IDomainEvent> domainEvents)
+    private IEnumerable<EventGridEvent> GetEventsList(string subject, IEnumerable<IDomainEvent> domainEvents)
     {
         // TODO: Propagate correlation ID by setting data with "telemetryCorrelationId" property when OpenTelemetry is fully supported
         return domainEvents.Select(domainEvent => new EventGridEvent(
-            subject: $"{topic}-{subject}",
+            subject: $"{this.TopicName}-{subject}",
             eventType: domainEvent.GetType().FullName,
             dataVersion: domainEvent.DataVersion,
             data: new BinaryData(domainEvent, SerializerOptions)));
