49f82f921c4f50934aeb7ff360f0979257fe6e42
Guillaume Caya-Letourneau <guillaume.caya@gsoft.com>
2023-07-26 16:03:37 -0400

    Removed unwanted projects and moved some interfaces to abstractions project

--- src/Workleap.DomainEventPropagation.Subscription/IEventGridRequestHandler.cs
+++ src/Workleap.DomainEventPropagation.Subscription/IEventGridRequestHandler.cs
@@ -2,7 +2,7 @@ using Microsoft.ApplicationInsights.DataContracts;
 
 namespace Workleap.DomainEventPropagation;
 
-public interface IEventGridRequestHandler
+internal interface IEventGridRequestHandler
 {
     Task<EventGridRequestResult> HandleRequestAsync(object requestContent, CancellationToken cancellationToken, RequestTelemetry requestTelemetry = default);
 }
