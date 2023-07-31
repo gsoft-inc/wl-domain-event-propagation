242060d74da6cee46f2fd9b84ca08ad77e3acd49
Xavier Tremblay <zavier.tremblay@gmail.com>
2023-07-31 15:01:56 -0400

    Feature/idp 452 add rbac (#19)
    * Initial commit for RBAC
    
    * Merge main
    
    * Fix a merge issue

--- src/Workleap.DomainEventPropagation.Publishing/EventPropagationPublisherOptionsValidator.cs
+++ src/Workleap.DomainEventPropagation.Publishing/EventPropagationPublisherOptionsValidator.cs
@@ -13,6 +13,11 @@ public sealed class EventPropagationPublisherOptionsValidator : IValidateOptions
 
     public ValidateOptionsResult Validate(string name, EventPropagationPublisherOptions options)
     {
+        if (options.TokenCredential is null && string.IsNullOrWhiteSpace(options.TopicAccessKey))
+        {
+            return ValidateOptionsResult.Fail($"A token credential or an access key is required");
+        }
+
         return this._topicProviders.Any(x => x.GetAllTopicsNames().Contains(options.TopicName)) ?
             ValidateOptionsResult.Success :
             ValidateOptionsResult.Fail($"Unrecognized topic: {options.TopicName}");
