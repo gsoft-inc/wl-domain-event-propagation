255d89b218b75818536e423ade3d6f6ca37cf35b
Rushikesh Vyas <rush.vyas@gsoft.com>
2023-08-01 11:58:47 -0400

    Uncommented from build.ps1

--- Build.ps1
+++ Build.ps1
@@ -25,10 +25,9 @@ Process {
         Exec { & dotnet test  -c Release --no-build --results-directory "$outputDir" --no-restore -l "trx" -l "console;verbosity=detailed" }
         Exec { & dotnet pack  -c Release --no-build -o "$outputDir" }
 
-        # TODO: Re-enable once packages are ready to be published
-        # if (($null -ne $env:NUGET_SOURCE ) -and ($null -ne $env:NUGET_API_KEY)) {
-        #     Exec { & dotnet nuget push "$nupkgsPath" -s $env:NUGET_SOURCE -k $env:NUGET_API_KEY }
-        # }
+        if (($null -ne $env:NUGET_SOURCE ) -and ($null -ne $env:NUGET_API_KEY)) {
+            Exec { & dotnet nuget push "$nupkgsPath" -s $env:NUGET_SOURCE -k $env:NUGET_API_KEY }
+        }
     }
     finally {
         Pop-Location
