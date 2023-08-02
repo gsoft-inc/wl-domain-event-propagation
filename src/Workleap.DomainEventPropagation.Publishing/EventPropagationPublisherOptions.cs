62d249b57bfd62030d523452112dfa6d0c3a34fa
Vadim Grayfer <vadim.grayfer@gsoft.com>
2023-08-02 16:58:09 -0400

    remove NoWarn

--- src/Workleap.DomainEventPropagation.Publishing/EventPropagationPublisherOptions.cs
+++ src/Workleap.DomainEventPropagation.Publishing/EventPropagationPublisherOptions.cs
@@ -11,13 +11,13 @@ public sealed class EventPropagationPublisherOptions
     internal const string ClientName = "EventPropagationClient";
 
     [Required]
-    public string TopicName { get; set; }
+    public string TopicName { get; set; } = string.Empty;
 
-    public string TopicAccessKey { get; set; }
+    public string TopicAccessKey { get; set; } = string.Empty;
 
     public TokenCredential? TokenCredential { get; set; }
 
     [Required]
     [UrlOfKind(UriKind.Absolute)]
-    public string TopicEndpoint { get; set; }
+    public string TopicEndpoint { get; set; } = string.Empty;
 }
