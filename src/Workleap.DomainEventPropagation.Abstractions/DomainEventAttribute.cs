96db37c78586fa9414d44f129469c02ebdb687ca
Anthony Simmon <anthony.simmon@gsoft.com>
2023-08-31 13:47:05 -0400

    Added domain event attribute and reflection cache

---
+++ src/Workleap.DomainEventPropagation.Abstractions/DomainEventAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Workleap.DomainEventPropagation;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public sealed class DomainEventAttribute : Attribute
+{
+    public DomainEventAttribute(string name)
+    {
+        this.Name = name ?? throw new ArgumentNullException(nameof(name));
+    }
+
+    public string Name { get; }
+}
---
+++ src/Workleap.DomainEventPropagation.Abstractions/DomainEventNameCache.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace Workleap.DomainEventPropagation;
+
+internal static class DomainEventNameCache
+{
+    private static readonly ConcurrentDictionary<Type, string> EventNameTypeMappings = new ConcurrentDictionary<Type, string>();
+
+    public static string GetName(Type domainEventType)
+    {
+        if (!IsConcreteDomainEventType(domainEventType))
+        {
+            throw new ArgumentException(domainEventType + " must be a concrete type that implements " + nameof(IDomainEvent));
+        }
+
+        return EventNameTypeMappings.GetOrAdd(domainEventType, static domainEventType =>
+        {
+            if (domainEventType.GetCustomAttribute<DomainEventAttribute>() is { } attribute)
+            {
+                return attribute.Name;
+            }
+
+            throw new ArgumentException(domainEventType + " must be decorated with " + nameof(DomainEventAttribute));
+        });
+    }
+
+    public static bool IsConcreteDomainEventType(Type type) => !type.IsAbstract && typeof(IDomainEvent).IsAssignableFrom(type);
+
+    public static string GetName<T>()
+        where T : IDomainEvent
+    {
+        return GetName(typeof(T));
+    }
+}
