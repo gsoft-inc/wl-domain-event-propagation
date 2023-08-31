9e6b81830fa099f1dfb4f1b477a19a0a781db667
Guillaume Caya-Letourneau <guillaume.caya@gsoft.com>
2023-08-31 14:27:34 -0400

    Changed wrapper implementation

--- src/DomainEventPropagation/DomainEventWrapper.cs
+++ src/DomainEventPropagation/DomainEventWrapper.cs
@@ -1,12 +1,35 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace Workleap.DomainEventPropagation;
 
-internal class DomainEventWrapper : IDomainEvent
+internal class DomainEventWrapper
 {
-    public string DomainEventType { get; set; } = string.Empty;
+    private const string DomainEventTypeKey = "__type";
+    private const string MetadataKey = "__metadata";
 
-    public JsonElement DomainEventJson { get; set; }
+    private readonly JsonNode _domainEvent;
 
-    public Dictionary<string, string> ExtensionAttributes { get; set; } = new();
+    public DomainEventWrapper(JsonNode domainEvent)
+    {
+        this._domainEvent = domainEvent;
+    }
+
+    public DomainEventWrapper(JsonNode domainEvent, string domainEventType)
+    {
+        this._domainEvent = domainEvent;
+        this.DomainEventType = domainEventType;
+    }
+
+    public string DomainEventType
+    {
+        get => this._domainEvent[DomainEventTypeKey]?.ToString() ?? string.Empty;
+        set => this._domainEvent[DomainEventTypeKey] = value;
+    }
+
+    public Dictionary<string, string> Metadata
+    {
+        get => (this._domainEvent[MetadataKey] ??= new JsonObject()).Deserialize<Dictionary<string, string>>()!;
+        set => this._domainEvent[MetadataKey] = JsonSerializer.SerializeToNode(value);
+    }
 }
