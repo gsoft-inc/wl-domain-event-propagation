6458b31163ca1a30622cdbfffdc470cc658bf02c
Guillaume Caya-Letourneau <guillaume.caya@gsoft.com>
2023-09-06 14:37:28 -0400

    Added event wrapper tests and json constants file

---
+++ src/Shared/JsonConstants.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json;
+
+namespace Workleap.DomainEventPropagation;
+
+internal static class JsonConstants
+{
+    public static readonly JsonSerializerOptions DomainEventWrapperSerializerOptions = new(JsonSerializerDefaults.Web);
+}
---
+++ src/Workleap.DomainEventPropagation.Publishing.Tests/DomainEventWrapperTests.cs
@@ -0,0 +1,68 @@
+﻿using Azure.Messaging.EventGrid;
+
+namespace Workleap.DomainEventPropagation.Publishing.Tests;
+
+public class DomainEventWrapperTests
+{
+    private readonly EventGridEvent _eventGridEvent = new EventGridEvent(
+        "subject",
+        "eventType",
+        "1.0",
+        new BinaryData(new SampleDomainEvent()));
+
+    private const string DomainEventName = "sample-event";
+
+    [Fact]
+    public void GivenEventGridEvent_WhenSetMetadata_ThenMetadataIsSet()
+    {
+        // Given
+        var eventWrapper = new DomainEventWrapper(this._eventGridEvent);
+
+        // When
+        eventWrapper.SetMetadata("someKey", "someValue");
+
+        // Then
+        var valueFound = eventWrapper.TryGetMetadata("someKey", out var value);
+
+        Assert.True(valueFound);
+        Assert.Equal("someValue", value);
+    }
+
+    [Fact]
+    public void GivenEventGridEventWithoutMetadata_WhenTryGetMetadata_ThenMetadataNotFound()
+    {
+        // Given
+        var eventWrapper = new DomainEventWrapper(this._eventGridEvent);
+
+        // When
+        var valueFound = eventWrapper.TryGetMetadata("somekey", out var value);
+
+        // Then
+        Assert.False(valueFound);
+        Assert.Null(value);
+    }
+
+    [Fact]
+    public void GivenDomainEvent_WhenWrapEvent_ThenEventWrappedProperly()
+    {
+        // Given
+        var domainEvent = new SampleDomainEvent() { Message = "Hello world" };
+
+        // When
+        var wrappedEvent = DomainEventWrapper.Wrap(domainEvent);
+
+        // Then
+        Assert.Equal(DomainEventName, wrappedEvent.DomainEventName);
+
+        var unwrappedEvent = (SampleDomainEvent)wrappedEvent.Unwrap(typeof(SampleDomainEvent));
+
+        Assert.NotNull(unwrappedEvent);
+        Assert.Equal(domainEvent.Message, unwrappedEvent.Message);
+    }
+
+    [DomainEvent(DomainEventName)]
+    private class SampleDomainEvent : IDomainEvent
+    {
+        public string? Message { get; set; }
+    }
+}
