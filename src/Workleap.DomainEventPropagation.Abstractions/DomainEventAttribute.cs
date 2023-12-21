bdb4d34aa3f08e9ecf2dbfeef99700682826292b
Damien Ardouin <damien.ardouin@gsoft.com>
2023-12-21 16:29:00 -0500

    Fixed analyzer related test

--- src/Workleap.DomainEventPropagation.Abstractions/DomainEventAttribute.cs
+++ src/Workleap.DomainEventPropagation.Abstractions/DomainEventAttribute.cs
@@ -3,6 +3,10 @@
 [AttributeUsage(AttributeTargets.Class, Inherited = false)]
 public sealed class DomainEventAttribute : Attribute
 {
+    public DomainEventAttribute(string name) : this(name, EventSchema.EventGridEvent)
+    {
+    }
+
     public DomainEventAttribute(string name, EventSchema eventSchema = EventSchema.EventGridEvent)
     {
         if (name == null)
