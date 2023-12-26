2c7706ec3248a9f6ea532bc8f27af8ddbcc8db16
Damien ardouin <da.ardouin@gmail.com>
2023-12-26 11:54:37 -0500

    Add option to support namespacetopic (#74)
    * Added option for topic type

--- src/Workleap.DomainEventPropagation.Publishing.Tests/EventPropagationPublisherOptionsValidatorTests.cs
+++ src/Workleap.DomainEventPropagation.Publishing.Tests/EventPropagationPublisherOptionsValidatorTests.cs
@@ -1,4 +1,4 @@
-#nullable disable // To reproduce users that don't have nullable enabled 
+#nullable disable // To reproduce users that don't have nullable enabled
 
 using Azure.Identity;
 
@@ -7,24 +7,31 @@ namespace Workleap.DomainEventPropagation.Publishing.Tests;
 public class EventPropagationPublisherOptionsValidatorTests
 {
     [Theory]
-    [InlineData(" ", "http://topicurl.com", true, true)]
-    [InlineData(" ", "http://topicurl.com", false, false)]
-    [InlineData("accessKey", "http://topicurl.com", true, true)]
-    [InlineData("accessKey", "http://topicurl.com", false, true)]
-    [InlineData(null, null, false, false)]
-    [InlineData(null, "http://topicurl.com", false, false)]
-    [InlineData("accessKey", " ", false, false)]
-    [InlineData("accessKey", null, false, false)]
-    [InlineData("accessKey", "topicEndpoint", false, false)]
-    public void GivenEventPropagationConfigurations_WhenValidateOptions_ThenAccessCredentialsValidated(string topicAccessKey, string topicEndpoint, bool useTokenCredential, bool optionsValid)
+    [InlineData(TopicType.Default, " ", "http://topicurl.com", "topicName", true, true)]
+    [InlineData(TopicType.Default, " ", "http://topicurl.com", "topicName", false, false)]
+    [InlineData(TopicType.Default, "accessKey", "http://topicurl.com", "topicName", true, true)]
+    [InlineData(TopicType.Default, "accessKey", "http://topicurl.com", "topicName", false, true)]
+    [InlineData(TopicType.Default, null, null, "topicName", false, false)]
+    [InlineData(TopicType.Default, null, "http://topicurl.com", "topicName", false, false)]
+    [InlineData(TopicType.Default, "accessKey", " ", "topicName", false, false)]
+    [InlineData(TopicType.Default, "accessKey", null, "topicName", false, false)]
+    [InlineData(TopicType.Default, "accessKey", "topicEndpoint", "topicName", false, false)]
+    [InlineData(TopicType.Default, "accessKey", "http://topicurl.com", null, true, true)]
+    [InlineData(TopicType.Default, "accessKey", "http://topicurl.com", " ", true, true)]
+    [InlineData(TopicType.Namespace, "accessKey", "http://topicurl.com", "topicName", true, true)]
+    [InlineData(TopicType.Namespace, "accessKey", "http://topicurl.com", "", true, false)]
+    [InlineData(TopicType.Namespace, "accessKey", "http://topicurl.com", null, true, false)]
+    public void GivenEventPropagationConfigurations_WhenValidateOptions_ThenAccessCredentialsValidated(TopicType topicType, string topicAccessKey, string topicEndpoint, string topicName, bool useTokenCredential, bool optionsValid)
     {
         var validator = new EventPropagationPublisherOptionsValidator();
 
         var result = validator.Validate("namedOptions", new EventPropagationPublisherOptions
         {
+            TopicType = topicType,
             TokenCredential = useTokenCredential ? new DefaultAzureCredential() : default,
             TopicEndpoint = topicEndpoint,
             TopicAccessKey = topicAccessKey,
+            TopicName = topicName,
         });
 
         Assert.True(optionsValid ? result.Succeeded : result.Failed);
