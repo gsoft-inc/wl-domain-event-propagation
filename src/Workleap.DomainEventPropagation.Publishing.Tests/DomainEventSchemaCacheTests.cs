ee79be44b9abe5504e0de661d62cdbb448709ea6
Damien ardouin <da.ardouin@gmail.com>
2023-12-26 11:23:37 -0500

    Added support for event schema in DomainEvent (#72)
    * Added support for event schema in DomainEvent
    * Updated action filter to run CI

---
+++ src/Workleap.DomainEventPropagation.Publishing.Tests/DomainEventSchemaCacheTests.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Messaging;
+using Azure.Messaging.EventGrid;
+
+namespace Workleap.DomainEventPropagation.Publishing.Tests;
+
+public class DomainEventSchemaCacheTests
+{
+    [Fact]
+    public void Given_EventGridEvent_When_RetrievingSchema_Then_ExpectedSchemaIsRetrieved()
+    {
+        // Arrange & Act
+        var eventGridSchema = DomainEventSchemaCache.GetEventSchema<EventGridSampleDomainEvent>();
+
+        // Assert
+        Assert.Equal(EventSchema.EventGridEvent, eventGridSchema);
+    }
+
+    [Fact]
+    public void Given_CloudEvent_When_RetrievingSchema_Then_ExpectedSchemaIsRetrieved()
+    {
+        // Arrange & Act
+        var cloudEventSchema = DomainEventSchemaCache.GetEventSchema<CloudEventSampleDomainEvent>();
+
+        // Assert
+        Assert.Equal(EventSchema.CloudEvent, cloudEventSchema);
+    }
+
+    [DomainEvent("sample-eg-event")]
+    private class EventGridSampleDomainEvent : IDomainEvent
+    {
+        public string? Message { get; set; }
+    }
+
+    [DomainEvent("sample-cloud-event", EventSchema.CloudEvent)]
+    private class CloudEventSampleDomainEvent : IDomainEvent
+    {
+        public string? Message { get; set; }
+    }
+}
