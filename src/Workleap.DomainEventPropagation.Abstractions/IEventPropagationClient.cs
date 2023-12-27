4bce23fb7980f0a3c544c8bcc3b8fa86efe6d97b
Damien ardouin <da.ardouin@gmail.com>
2023-12-27 11:34:34 -0500

    Updated Generic publication methods to force using a non-abstract event (#76)
    Now forces consumer to give a concrete event : before, it was silently failing when we were wrapping the event (we proceed to a check that it's not abstract when wrapping it).
    Passing abstract events to publication methods will create a build-time error (instead of runtime).

--- src/Workleap.DomainEventPropagation.Abstractions/IEventPropagationClient.cs
+++ src/Workleap.DomainEventPropagation.Abstractions/IEventPropagationClient.cs
@@ -3,8 +3,8 @@ namespace Workleap.DomainEventPropagation;
 public interface IEventPropagationClient
 {
     Task PublishDomainEventAsync<T>(T domainEvent, CancellationToken cancellationToken)
-        where T : IDomainEvent;
+        where T : IDomainEvent, new();
 
     Task PublishDomainEventsAsync<T>(IEnumerable<T> domainEvents, CancellationToken cancellationToken)
-        where T : IDomainEvent;
+        where T : IDomainEvent, new();
 }
