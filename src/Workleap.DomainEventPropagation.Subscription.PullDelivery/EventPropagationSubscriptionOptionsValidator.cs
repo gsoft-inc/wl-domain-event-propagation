5b140182ceb5bdcbe24658b04403f399f3049232
Damien ardouin <da.ardouin@gmail.com>
2024-01-08 10:36:03 -0500

    Added options for subscriber (pull model) (#80)
    Extension/Builder was used to follow what was already done for publisher / push model subscriber.
    
    Added options plus validation for Subscriber (pull model)
    Named options are supported so that we can register multiple subscribers to different topics
    Added extension method and builder to register those options

---
+++ src/Workleap.DomainEventPropagation.Subscription.PullDelivery/EventPropagationSubscriptionOptionsValidator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Workleap.DomainEventPropagation;
+
+public class EventPropagationSubscriptionOptionsValidator : IValidateOptions<EventPropagationSubscriptionOptions>
+{
+    public ValidateOptionsResult Validate(string name, EventPropagationSubscriptionOptions options)
+    {
+        if (options.TokenCredential is null && string.IsNullOrWhiteSpace(options.TopicAccessKey))
+        {
+            return ValidateOptionsResult.Fail("A token credential or an access key is required");
+        }
+
+        if (string.IsNullOrWhiteSpace(options.TopicEndpoint))
+        {
+            return ValidateOptionsResult.Fail("A topic endpoint is required");
+        }
+
+        if (!Uri.TryCreate(options.TopicEndpoint, UriKind.Absolute, out _))
+        {
+            return ValidateOptionsResult.Fail("The topic endpoint must be an absolute URI");
+        }
+
+        if (string.IsNullOrWhiteSpace(options.TopicName))
+        {
+            return ValidateOptionsResult.Fail("A topic endpoint is required");
+        }
+
+        if (string.IsNullOrWhiteSpace(options.SubscriptionName))
+        {
+            return ValidateOptionsResult.Fail("A topic endpoint is required");
+        }
+
+        return ValidateOptionsResult.Success;
+    }
+}
---
+++ src/Workleap.DomainEventPropagation.Subscription.PullDelivery/IEventPropagationSubscriberBuilder.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Workleap.DomainEventPropagation;
+
+public interface IEventPropagationSubscriberBuilder
+{
+    IServiceCollection Services { get; }
+}
