873cf8ef4844bdfee0295398d8f48add03b089c8
Damien ardouin <da.ardouin@gmail.com>
2024-01-08 10:36:03 -0500

    Added options for subscriber (pull model) (#80)
    Extension/Builder was used to follow what was already done for publisher / push model subscriber.
    
    Added options plus validation for Subscriber (pull model)
    Named options are supported so that we can register multiple subscribers to different topics
    Added extension method and builder to register those options

---
+++ src/Workleap.DomainEventPropagation.Subscription.PullDelivery/EventPropagationSubscriberBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace Workleap.DomainEventPropagation;
+
+internal sealed class EventPropagationSubscriberBuilder : IEventPropagationSubscriberBuilder
+{
+    public EventPropagationSubscriberBuilder(IServiceCollection services)
+    {
+        this.Services = services;
+    }
+
+    public IServiceCollection Services { get; }
+}
