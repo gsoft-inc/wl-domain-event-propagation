77eb2f6185a165beb45be4f2703f178d4373494a
Damien ardouin <da.ardouin@gmail.com>
2024-01-08 13:44:46 -0500

    Wrapped cloud events when pulled and added delegates to handle event lifetime (#83)
    * Added a cloud event handler that is called by puller to wrap cloud event
    * Changed EventGridClient wrapper to apply adapter pattern
    * Removed nullable on lock token, renamed some tests

---
+++ src/Workleap.DomainEventPropagation.Subscription.PullDelivery/CloudEventHandler.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Messaging;
+using Microsoft.Extensions.Logging;
+
+namespace Workleap.DomainEventPropagation;
+
+internal sealed class CloudEventHandler : ICloudEventHandler
+{
+    private readonly IDomainEventTypeRegistry _domainEventTypeRegistry;
+    private readonly ILogger<CloudEventHandler> _logger;
+    private readonly DomainEventHandlerDelegate _pipeline;
+
+    public CloudEventHandler(
+        IDomainEventTypeRegistry domainEventTypeRegistry,
+        IEnumerable<IDomainEventBehavior> domainEventBehaviors,
+        ILogger<CloudEventHandler> logger)
+    {
+        this._domainEventTypeRegistry = domainEventTypeRegistry;
+        this._logger = logger;
+        this._pipeline = domainEventBehaviors.Reverse().Aggregate((DomainEventHandlerDelegate)HandleDomainEventAsync, BuildPipeline);
+    }
+
+    public async Task<EventProcessingStatus> HandleCloudEventAsync(CloudEvent cloudEvent, CancellationToken cancellationToken)
+    {
+        var domainEventWrapper = new DomainEventWrapper(cloudEvent);
+
+        var domainEventType = this._domainEventTypeRegistry.GetDomainEventType(domainEventWrapper.DomainEventName);
+        if (domainEventType == null)
+        {
+            this._logger.EventDomainTypeNotRegistered(domainEventWrapper.DomainEventName, cloudEvent.Subject ?? "Unknown");
+            return EventProcessingStatus.Rejected;
+        }
+
+        return await this._pipeline(domainEventWrapper, cancellationToken).ConfigureAwait(false);
+    }
+
+    private static DomainEventHandlerDelegate BuildPipeline(DomainEventHandlerDelegate next, IDomainEventBehavior pipeline)
+    {
+        return (@event, cancellationToken) => pipeline.HandleAsync(@event, next, cancellationToken);
+    }
+
+    private static Task<EventProcessingStatus> HandleDomainEventAsync(
+        DomainEventWrapper domainEventWrapper,
+        CancellationToken cancellationToken)
+    {
+        // Todo : Get event handler that matches wrapper type and invoke it
+        return Task.FromResult(EventProcessingStatus.Handled);
+    }
+}
---
+++ src/Workleap.DomainEventPropagation.Subscription.PullDelivery/EventGridClientAdapter/EventGridClientAdapter.cs
@@ -0,0 +1,40 @@
+﻿using Azure.Messaging;
+using Azure.Messaging.EventGrid.Namespaces;
+
+namespace Workleap.DomainEventPropagation.EventGridClientAdapter;
+
+internal sealed class EventGridClientAdapter : IEventGridClientAdapter
+{
+    private readonly EventGridClient _adaptee;
+
+    public EventGridClientAdapter(EventGridClient adaptee)
+    {
+        this._adaptee = adaptee;
+    }
+
+    public async Task<IEnumerable<EventBundle>> ReceiveCloudEventsAsync(string topicName, string eventSubscriptionName, CancellationToken cancellationToken)
+    {
+        var result = await this._adaptee.ReceiveCloudEventsAsync(topicName, eventSubscriptionName, cancellationToken: cancellationToken).ConfigureAwait(false);
+
+        return result.HasValue
+            ? result.Value.Value.Select(x => new EventBundle(x.Event, x.BrokerProperties.LockToken))
+            : Array.Empty<EventBundle>();
+    }
+
+    public Task AcknowledgeCloudEventAsync(string topicName, string eventSubscriptionName, string lockToken, CancellationToken cancellationToken)
+    {
+        return this._adaptee.AcknowledgeCloudEventsAsync(topicName, eventSubscriptionName, new AcknowledgeOptions(new[] { lockToken }), cancellationToken);
+    }
+
+    public Task ReleaseCloudEventAsync(string topicName, string eventSubscriptionName, string lockToken, CancellationToken cancellationToken)
+    {
+        return this._adaptee.ReleaseCloudEventsAsync(topicName, eventSubscriptionName, new ReleaseOptions(new[] { lockToken }), cancellationToken: cancellationToken);
+    }
+
+    public Task RejectCloudEventAsync(string topicName, string eventSubscriptionName, string lockToken, CancellationToken cancellationToken)
+    {
+        return this._adaptee.RejectCloudEventsAsync(topicName, eventSubscriptionName, new RejectOptions(new[] { lockToken }), cancellationToken);
+    }
+
+    internal sealed record EventBundle(CloudEvent Event, string LockToken);
+}
