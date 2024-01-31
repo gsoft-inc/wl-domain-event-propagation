b94fc614af0347cbbd0336aeb76171643026377c
Damien ardouin <da.ardouin@gmail.com>
2024-01-31 17:43:34 -0500

    Added support to register event handlers (#95)
    * Added a way to register domain event handlers using shared extension methods / helper
    * Added a misconfigured dll for test purpose (multiple handlers)

---
+++ src/Workleap.DomainEventPropagation.Subscription.PullDelivery.Tests/Events/SampleEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Concurrent;
+
+namespace Workleap.DomainEventPropagation.Subscription.PullDelivery.Tests.Events;
+
+[DomainEvent(Constants.SampleEventName, EventSchema.CloudEvent)]
+public class SampleEvent : IDomainEvent
+{
+    public string? Message { get; set; }
+}
+
+public class SampleEventTestHandler : IDomainEventHandler<SampleEvent>
+{
+    public static ConcurrentQueue<SampleEvent> ReceivedEvents { get; } = new();
+
+    public Task HandleDomainEventAsync(SampleEvent domainEvent, CancellationToken cancellationToken)
+    {
+        ReceivedEvents.Enqueue(domainEvent);
+        return Task.CompletedTask;
+    }
+}
