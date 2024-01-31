2910623a76c7f02b0775f4dc6791045b35a92660
Damien ardouin <da.ardouin@gmail.com>
2024-01-31 17:29:29 -0500

    Extracted assembly helpers (#94)
    * Moved methods used for handler registration in shared static helper
    * Added missing extension methods for handler registration

---
+++ src/Shared/AssemblyHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace Workleap.DomainEventPropagation;
+
+internal static class AssemblyHelper
+{
+    public static IEnumerable<Type> GetConcreteHandlerTypes(Assembly assembly)
+    {
+        if (assembly == null)
+        {
+            throw new ArgumentNullException(nameof(assembly));
+        }
+
+        return assembly.GetTypes().Where(IsConcreteDomainEventHandlerType);
+    }
+
+    public static bool IsIDomainEventHandler(Type type)
+    {
+        return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IDomainEventHandler<>);
+    }
+
+    private static bool IsConcreteDomainEventHandlerType(Type type)
+    {
+        return !type.IsAbstract && type.GetInterfaces().Any(IsIDomainEventHandler);
+    }
+}
