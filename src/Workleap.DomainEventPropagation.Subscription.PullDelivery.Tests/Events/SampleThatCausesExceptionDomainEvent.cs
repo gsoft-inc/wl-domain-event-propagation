5680407d7d8b8e96d99ac2fdcfb6a68802daa59e
Damien ardouin <da.ardouin@gmail.com>
2024-02-02 08:55:35 -0500

    Retrieve and call handlers when cloud event is received (#96)
    * Now retrieve and call handlers when cloud event is received

---
+++ src/Workleap.DomainEventPropagation.Subscription.PullDelivery.Tests/Events/SampleThatCausesExceptionDomainEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace Workleap.DomainEventPropagation.Subscription.PullDelivery.Tests.Events;
+
+[DomainEvent("sample-event-that-causes-exception", EventSchema.CloudEvent)]
+public class SampleThatCausesExceptionDomainEvent : IDomainEvent
+{
+    public string? Message { get; set; }
+}
+
+public class SampleThatCausesExceptionDomainEventHandler : IDomainEventHandler<SampleThatCausesExceptionDomainEvent>
+{
+    public Task HandleDomainEventAsync(SampleThatCausesExceptionDomainEvent domainEvent, CancellationToken cancellationToken)
+    {
+        throw new Exception();
+    }
+}
