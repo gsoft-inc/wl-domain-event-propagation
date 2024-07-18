e7d33138350ad9fe3d3d745d8d86a0d687acf3aa
Jonathan Melo <jonathan.melo@gsoft.com>
2024-07-18 14:11:07 -0400

    Safety check for emptiness

--- src/Workleap.DomainEventPropagation.Subscription.PullDelivery/EventPullerService.cs
+++ src/Workleap.DomainEventPropagation.Subscription.PullDelivery/EventPullerService.cs
@@ -67,7 +67,7 @@ internal sealed class EventPullerService : BackgroundService
             this._logger = logger;
             this._eventGridTopicSubscription = eventGridTopicSubscription;
 
-            if (eventGridTopicSubscription.RetryDelays != null)
+            if (eventGridTopicSubscription.RetryDelays != null && eventGridTopicSubscription.RetryDelays.Any())
             {
                 this._retryDelays = eventGridTopicSubscription.RetryDelays
                     .Select((x, index) => new { DeliveryCount = index + 1, Delay = x })
