c6d5b4495fe27e92dfb96e01599aee2a463ae236
Mathieu Gamache <mathieu.gamache@gsoft.com>
2024-10-08 15:55:56 -0400

    Fix CI

--- src/Workleap.DomainEventPropagation.Subscription.PullDelivery.Tests/EventPropagationSubscriptionOptionsValidatorTests.cs
+++ src/Workleap.DomainEventPropagation.Subscription.PullDelivery.Tests/EventPropagationSubscriptionOptionsValidatorTests.cs
@@ -12,28 +12,28 @@ public class EventPropagationSubscriptionOptionsValidatorTests
     [InlineData("accessKey", false, "http://topicurl.com", "topicName", "subName", null, true)]
 
     // Access key
-    [InlineData(" ", false, "http://topicurl.com", "topicName", "subName", null,false)]
-    [InlineData(null, false, "http://topicurl.com", "topicName", "subName", null,false)]
+    [InlineData(" ", false, "http://topicurl.com", "topicName", "subName", null, false)]
+    [InlineData(null, false, "http://topicurl.com", "topicName", "subName", null, false)]
 
     // Token credential
     [InlineData("accessKey", true, "http://topicurl.com", "topicName", "subName", null, true)]
 
     // Topic endpoint
-    [InlineData("accessKey", false, "invalid-url", "topicName", "subName", null,false)]
+    [InlineData("accessKey", false, "invalid-url", "topicName", "subName", null, false)]
     [InlineData("accessKey", false, null, "topicName", "subName", null, false)]
     [InlineData("accessKey", false, " ", "topicName", "subName", null, false)]
 
     // Topic name
     [InlineData("accessKey", false, "http://topicurl.com", " ", "subName", null, false)]
-    [InlineData("accessKey", false, "http://topicurl.com", null, "subName",null, false)]
+    [InlineData("accessKey", false, "http://topicurl.com", null, "subName", null, false)]
 
     // Subscription name
-    [InlineData("accessKey", false, "http://topicurl.com", "topicName", "", null,false)]
-    [InlineData("accessKey", false, "http://topicurl.com", "topicName", null,null, false)]
+    [InlineData("accessKey", false, "http://topicurl.com", "topicName", "", null, false)]
+    [InlineData("accessKey", false, "http://topicurl.com", "topicName", null, null, false)]
 
     // Max retries count
-    [InlineData("accessKey", false, "http://topicurl.com", "topicName", "", -1,false)]
-    [InlineData("accessKey", false, "http://topicurl.com", "topicName", null,11, false)]
+    [InlineData("accessKey", false, "http://topicurl.com", "topicName", "", -1, false)]
+    [InlineData("accessKey", false, "http://topicurl.com", "topicName", null, 11, false)]
     public void GivenNamedConfiguration_WhenValidate_ThenOptionsAreValidated(string topicAccessKey, bool useTokenCredential, string topicEndpoint, string topicName, string subName, int? maxRetriesCount, bool validationSucceeded)
     {
         var option = new EventPropagationSubscriptionOptions
@@ -42,17 +42,17 @@ public class EventPropagationSubscriptionOptionsValidatorTests
             TopicEndpoint = topicEndpoint,
             TopicAccessKey = topicAccessKey,
             TopicName = topicName,
-            SubscriptionName = subName,
+            SubscriptionName = subName
         };
 
-        if(maxRetriesCount.HasValue)
+        if (maxRetriesCount.HasValue)
         {
             option.MaxRetries = maxRetriesCount.Value;
         }
 
         var validator = new EventPropagationSubscriptionOptionsValidator();
-        var result = validator.Validate("namedOptions", option);
+        var result = validator.Validate(name: "namedOptions", options: option);
 
-        Assert.Equal(validationSucceeded, result.Succeeded);
+        Assert.Equal(expected: validationSucceeded, actual: result.Succeeded);
     }
 }
